98d56bdcb9e5cac625194df4e97b3aab19894a69
Oratakashi Nhamako <davidcreazy@gmail.com>
2019-12-12 21:40:42 +0700

    Update a316a79ed6bd5479b2e01d0208f1acad1399e907.php

--- application/cache/a316a79ed6bd5479b2e01d0208f1acad1399e907.php
+++ application/cache/a316a79ed6bd5479b2e01d0208f1acad1399e907.php
@@ -2,38 +2,38 @@
 
 <?php $__env->startSection('container'); ?>
 <section id="main-content">
-          <section class="wrapper">
-              <!-- page start-->
-                <div class="row">
-                    <div class="col-sm-12">
-                        <section class="card">
-                            <div class="card-body">
-                                <ul class="nav nav-tabs mb-2" id="myTab" role="tablist">
-                                  <li class="nav-item">
-                                      <a class="nav-link active" id="home-tab" data-toggle="tab" href="#periode" role="tab" aria-controls="periode" aria-selected="true">Rekap Per Periode</a>
-                                  </li>
-                                  <li class="nav-item">
-                                      <a class="nav-link" id="profile-tab" data-toggle="tab" href="#bulan" role="tab" aria-controls="bulan" aria-selected="false">Rekap Per Bulan</a>
-                                  </li>
-                                  <li class="nav-item">
-                                      <a class="nav-link" id="contact-tab" data-toggle="tab" href="#tahun" role="tab" aria-controls="tahun" aria-selected="false">Rekap Per Tahun</a>
-                                  </li>
-                                  <li class="nav-item">
-                                      <a class="nav-link" id="contact-tab" data-toggle="tab" href="#all" role="tab" aria-controls="all" aria-selected="false">Rekap Sepanjang masa</a>
-                                  </li>
-                              </ul>
-                              <div class="tab-content" id="myTabContent">
-                                  <?php echo $__env->make('backend.summary.periode', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                                  <?php echo $__env->make('backend.summary.bulan', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                                  <?php echo $__env->make('backend.summary.tahun', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                                  <?php echo $__env->make('backend.summary.all', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                              </div>
-                            </div>
-                        </section>
+    <section class="wrapper">
+        <!-- page start-->
+        <div class="row">
+            <div class="col-sm-12">
+                <section class="card">
+                    <div class="card-body">
+                        <ul class="nav nav-tabs mb-2" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#periode" role="tab" aria-controls="periode" aria-selected="true">Rekap Per Periode</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#bulan" role="tab" aria-controls="bulan" aria-selected="false">Rekap Per Bulan</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#tahun" role="tab" aria-controls="tahun" aria-selected="false">Rekap Per Tahun</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#all" role="tab" aria-controls="all" aria-selected="false">Rekap Sepanjang masa</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <?php echo $__env->make('backend.summary.periode', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                            <?php echo $__env->make('backend.summary.bulan', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                            <?php echo $__env->make('backend.summary.tahun', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                            <?php echo $__env->make('backend.summary.all', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                        </div>
                     </div>
-                </div>
-          </section>
-      </section>
+                </section>
+            </div>
+        </div>
+    </section>
+</section>
       <?php echo $__env->make('backend.summary.periode_script', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
       <?php echo $__env->make('backend.summary.bulan_script', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
       <?php echo $__env->make('backend.summary.tahun_script', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
