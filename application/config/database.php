78435d8239a5d01e41835969e6bed7098344805a
Oratakashi Nhamako <davidcreazy@gmail.com>
2019-12-13 09:17:58 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,28 +73,70 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	// 'hostname' => 'localhost',
-	// 'username' => 'root',
-	// 'password' => '',
-	// 'database' => 'db_pramuka',
-	'hostname' => 'pramukalumajang.or.id:3306',
-	'username' => 'u3562650_root',
-	'password' => 'admin12',
-	'database' => 'u3562650_webbaru',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+if ($_SERVER['HTTP_HOST'] == 'localhost'){
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'pramukalumajang.or.id:3306',
+		'username' => 'u3562650_root',
+		'password' => 'admin12',
+		'database' => 'u3562650_webbaru',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);
+}elseif($_SERVER['HTTP_HOST'] == 'dev.pramukalumajang.or.id'){
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'pramukalumajang.or.id:3306',
+		'username' => 'u3562650_root',
+		'password' => 'admin12',
+		'database' => 'u3562650_webbaru',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);
+}else{
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'pramukalumajang.or.id:3306',
+		'username' => 'u3562650_root',
+		'password' => 'admin12',
+		'database' => 'u3562650_production',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);
+}
